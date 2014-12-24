de379e8be617eeeda968407bbb0cfb1935b34e1f
Jimmy Yang <gankkank@gmail.com>
2014-12-24 15:49:13 +0800

    init project

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'mysql'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures mysql'
+long_description 'Installs/Configures mysql'
+version          '0.1.0'
+
